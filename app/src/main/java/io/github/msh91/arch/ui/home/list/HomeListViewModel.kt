a675358f2fe06256789107a53d8cf09132eee564
Mohammad Sharifi <sharifi.m91@gmail.com>
2018-05-26 01:37:39 +0430

    create domain package with CreateUserUseCase. create RepositoryBuilder module for providing repository instances

--- app/src/main/java/io/github/msh91/arch/ui/home/list/HomeListViewModel.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/list/HomeListViewModel.kt
@@ -1,11 +1,27 @@
 package io.github.msh91.arch.ui.home.list
 
+import android.util.Log
+import io.github.msh91.arch.data.model.error.ErrorModel
+import io.github.msh91.arch.data.model.user.TestUserModel
+import io.github.msh91.arch.domain.user.CreateUserUseCase
 import io.github.msh91.arch.ui.base.BaseViewModel
 import io.github.msh91.arch.util.connectivity.BaseConnectionManager
 import javax.inject.Inject
 
-class HomeListViewModel
-@Inject constructor(connectionManager: BaseConnectionManager): BaseViewModel(connectionManager) {
-
+class HomeListViewModel @Inject constructor(
+        private val createUserUseCase: CreateUserUseCase,
+        connectionManager: BaseConnectionManager
+): BaseViewModel(connectionManager) {
+    private val TAG = HomeListViewModel::class.java.simpleName
     override fun clearUseCaseDisposables() {}
+
+    override fun onStart() {
+        createUserUseCase
+                .setParameters(TestUserModel("name", "family"))
+                .execute(this::onResponse)
+    }
+
+    private fun onResponse(testUserModel: TestUserModel?, errorModel: ErrorModel?) {
+        Log.d(TAG, "onResponse() called  with: testUserModel = [$testUserModel], errorModel = [$errorModel]")
+    }
 }
