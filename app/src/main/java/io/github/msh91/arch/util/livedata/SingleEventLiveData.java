7479c1c82c626c8b81b58f897f4b47d0a272c0bf
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2018-05-26 11:31:43 +0430

    [FEATURE] create SingleEventLiveData [REFACTOR] move UiActionLiveData and NonNullLiveData to util/livedata package [UPDATE] change UiActionLiveData parent to SingleEventLiveData [TEST] create unit tests of HomeViewModel

---
+++ app/src/main/java/io/github/msh91/arch/util/livedata/SingleEventLiveData.java
@@ -0,0 +1,35 @@
+package io.github.msh91.arch.util.livedata;
+
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by sharifi on 3/10/18.
+ */
+
+/**
+ * Custom wrapper for {@link MutableLiveData} that calls observer only one time
+ * @param <T>
+ */
+public class SingleEventLiveData<T> extends MutableLiveData<T> {
+    private AtomicBoolean mPending = new AtomicBoolean(false);
+
+    @Override
+    public void setValue(T value) {
+        mPending.set(true);
+        super.setValue(value);
+    }
+
+    @Override
+    public void observe(@NonNull LifecycleOwner owner, @NonNull Observer<T> observer) {
+        super.observe(owner, t -> {
+            if (mPending.compareAndSet(true, false)) {
+                observer.onChanged(t);
+            }
+        });
+    }
+}
