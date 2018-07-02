7d722a2559d90ac1042aec9c2635f231166b6d79
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2018-07-02 10:49:45 +0430

    Use Lazy getter for viewModels in BaseFragment and BaseActivity instead of load them via Reflection.

--- app/src/main/java/io/github/msh91/arch/ui/home/HomeActivity.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/HomeActivity.kt
@@ -7,4 +7,5 @@ import io.github.msh91.arch.ui.base.BaseActivity
 class HomeActivity : BaseActivity<HomeViewModel, ActivityHomeBinding>() {
 
     override val layoutId: Int = R.layout.activity_home
+    override val viewModel: HomeViewModel by getLazyViewModel(this)
 }
