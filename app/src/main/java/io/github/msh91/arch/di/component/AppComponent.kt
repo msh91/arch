82915f72ed3b2d4819187c124f86856987d58f1a
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2018-07-02 11:26:00 +0430

    Create DatabaseModule class to provide database and it's dao interfaces via dagger and add module to AppComponent

--- app/src/main/java/io/github/msh91/arch/di/component/AppComponent.kt
+++ app/src/main/java/io/github/msh91/arch/di/component/AppComponent.kt
@@ -7,6 +7,7 @@ import dagger.android.AndroidInjectionModule
 import io.github.msh91.arch.app.ArchApplication
 import io.github.msh91.arch.di.builder.ActivityBuilder
 import io.github.msh91.arch.di.module.AppModule
+import io.github.msh91.arch.di.module.DatabaseModule
 import io.github.msh91.arch.di.module.NetworkModule
 import javax.inject.Singleton
 
@@ -18,6 +19,7 @@ import javax.inject.Singleton
     (AppModule::class),
     (NetworkModule::class),
     (AndroidInjectionModule::class),
+    (DatabaseModule::class),
     (ActivityBuilder::class)])
 interface AppComponent {
     @Component.Builder
