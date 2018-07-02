f2148ad17ac341d1835fd5d4977454c580ab9876
Mohammad sharifi <sharifi.m91@gmail.com>
2018-07-02 11:26:00 +0430

    Create DatabaseModule class to provide database and it's dao interfaces via dagger and add module to AppComponent

---
+++ app/src/main/java/io/github/msh91/arch/di/module/DatabaseModule.kt
@@ -0,0 +1,24 @@
+package io.github.msh91.arch.di.module
+
+import android.app.Application
+import android.arch.persistence.room.Room
+import dagger.Module
+import dagger.Provides
+import io.github.msh91.arch.data.source.db.AppDataBase
+import io.github.msh91.arch.data.source.db.dao.MovieDao
+import javax.inject.Singleton
+
+@Module
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideRoomDatabase(application: Application): AppDataBase {
+        return Room.databaseBuilder(application, AppDataBase::class.java, AppDataBase.DB_NAME).build()
+    }
+
+    @Provides
+    fun provideMovieDao(appDataBase: AppDataBase): MovieDao {
+        return appDataBase.movieDao()
+    }
+}
