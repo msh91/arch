619dc303b6affbad229de9c5fcc26ff1c0e85354
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2018-07-26 00:37:03 +0430

    Call GetAllMoviesUseCase from HomeListViewModel

--- app/src/main/java/io/github/msh91/arch/ui/home/list/HomeListViewModel.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/list/HomeListViewModel.kt
@@ -1,11 +1,25 @@
 package io.github.msh91.arch.ui.home.list
 
+import android.util.Log
+import io.github.msh91.arch.domain.model.movie.Movie
+import io.github.msh91.arch.domain.model.response.UseCaseResponse
+import io.github.msh91.arch.domain.usecase.GetAllMoviesUseCase
 import io.github.msh91.arch.ui.base.BaseViewModel
 import io.github.msh91.arch.util.connectivity.BaseConnectionManager
 import javax.inject.Inject
 
 class HomeListViewModel @Inject constructor(
-        connectionManager: BaseConnectionManager
+        connectionManager: BaseConnectionManager,
+        private val getAllMoviesUseCase: GetAllMoviesUseCase
 ) : BaseViewModel(connectionManager) {
     private val TAG = HomeListViewModel::class.java.simpleName
+
+    init {
+        getAllMoviesUseCase
+                .execute(compositeDisposable, this::getAllMoviesResponse, this::onTokenExpired)
+    }
+
+    private fun getAllMoviesResponse(response: UseCaseResponse<List<Movie>>) {
+        Log.d(TAG, "getAllMoviesResponse() called  with: response = [$response]")
+    }
 }
