7ad9dabcbc5493429a7f8fec41e54f6e0f131b69
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2018-07-26 00:35:14 +0430

    Define getAllMovies api in cloud repository and provide it's mock implementation

--- app/src/main/java/io/github/msh91/arch/data/source/cloud/CloudRepository.kt
+++ app/src/main/java/io/github/msh91/arch/data/source/cloud/CloudRepository.kt
@@ -2,6 +2,8 @@ package io.github.msh91.arch.data.source.cloud
 
 import io.github.msh91.arch.data.restful.APIs
 import io.github.msh91.arch.data.restful.APIsWithToken
+import io.github.msh91.arch.domain.model.movie.Movie
+import io.reactivex.Flowable
 
 /**
  * The main implementation of [BaseCloudRepository] that call api services directly
@@ -9,5 +11,7 @@ import io.github.msh91.arch.data.restful.APIsWithToken
  * @param apIsWithToken instance of with-token apis
  */
 class CloudRepository(private val apIs: APIs, private val apIsWithToken: APIsWithToken) : BaseCloudRepository {
-
+    override fun getAllMovies(): Flowable<List<Movie>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
