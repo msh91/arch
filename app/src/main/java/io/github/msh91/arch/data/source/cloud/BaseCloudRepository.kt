22ba527fe9214d44ac99e55b7639e402668416c2
Mohammad Sharifi <sharifi.m91@gmail.com>
2018-07-26 00:35:14 +0430

    Define getAllMovies api in cloud repository and provide it's mock implementation

--- app/src/main/java/io/github/msh91/arch/data/source/cloud/BaseCloudRepository.kt
+++ app/src/main/java/io/github/msh91/arch/data/source/cloud/BaseCloudRepository.kt
@@ -1,8 +1,12 @@
 package io.github.msh91.arch.data.source.cloud
 
+import io.github.msh91.arch.domain.model.movie.Movie
+import io.reactivex.Flowable
+
 /**
  * Every api service defined here as a contract and will be implemented for real lifecycle in
  * [CloudRepository] and for mock lifecycle in [CloudMockRepository]
  */
 interface BaseCloudRepository {
+    fun getAllMovies(): Flowable<List<Movie>>
 }
--- app/src/main/java/io/github/msh91/arch/data/source/cloud/CloudMockRepository.kt
+++ app/src/main/java/io/github/msh91/arch/data/source/cloud/CloudMockRepository.kt
@@ -1,5 +1,8 @@
 package io.github.msh91.arch.data.source.cloud
 
+import io.github.msh91.arch.domain.model.movie.Movie
+import io.reactivex.Flowable
+
 /**
  * Mock implementation of [BaseCloudRepository].
  *
@@ -7,4 +10,11 @@ package io.github.msh91.arch.data.source.cloud
  */
 class CloudMockRepository : BaseCloudRepository {
 
+    override fun getAllMovies(): Flowable<List<Movie>> {
+        return Flowable.just(listOf(
+                Movie(1, "Batman Begins!"),
+                Movie(2, "Batman: The Dark Knight"),
+                Movie(3, "Batman: The Dark Knight Rises")
+        ))
+    }
 }
