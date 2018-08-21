07901ce44b050d514e99797d7a5fafe20b5e1e5b
Mohammad sharifi <sharifi.m91@gmail.com>
2018-08-21 10:46:06 +0430

    Provide inject annotated constructor for util classes

--- app/src/main/java/io/github/msh91/arch/util/device/DeviceUtilImpl.kt
+++ app/src/main/java/io/github/msh91/arch/util/device/DeviceUtilImpl.kt
@@ -3,16 +3,18 @@ package io.github.msh91.arch.util.device
 import io.github.msh91.arch.BuildConfig
 import io.github.msh91.arch.data.source.preference.AppPreferencesHelper
 import java.util.*
+import javax.inject.Inject
 
-class DeviceUtilImpl(
-        private val appPreferencesHelper: AppPreferencesHelper) : BaseDeviceUtil {
+class DeviceUtilImpl @Inject constructor(
+        private val appPreferencesHelper: AppPreferencesHelper
+) : BaseDeviceUtil {
 
     override fun getAndroidVersion(): Int {
-        return  android.os.Build.VERSION.SDK_INT
+        return android.os.Build.VERSION.SDK_INT
     }
 
     override fun getUniqueId(): String {
-        if(appPreferencesHelper.uniqueId.isEmpty())
+        if (appPreferencesHelper.uniqueId.isEmpty())
             appPreferencesHelper.uniqueId = UUID.randomUUID().toString()
 
         return appPreferencesHelper.uniqueId
