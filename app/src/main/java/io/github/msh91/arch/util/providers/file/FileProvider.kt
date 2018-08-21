5e1324ab7ba79403bb8b3540688ec999aa987abe
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2018-08-21 10:21:16 +0430

    Create BaseFileProvider interface in order to do file related utils,  and provide it's default implementation

---
+++ app/src/main/java/io/github/msh91/arch/util/providers/file/FileProvider.kt
@@ -0,0 +1,38 @@
+package io.github.msh91.arch.util.providers.file
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.support.v4.content.FileProvider
+import java.io.File
+import java.io.InputStream
+import javax.inject.Inject
+
+class FileProvider @Inject constructor(private val context: Context) : BaseFileProvider {
+    override fun getUriForFile(context: Context, authority: String, file: File): Uri {
+        return FileProvider.getUriForFile(context, authority, file)
+    }
+
+    override fun makeShareCacheDirectory(): File {
+        return File(context.cacheDir, "/image").also {
+            if (it.exists()) it.delete()
+            it.mkdir()
+        }
+    }
+
+    override fun writeBitmapToFile(parent: File, bitmap: Bitmap, fileName: String): File {
+        return File(parent, fileName).also {
+            val out = it.outputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 85, out)
+            out.flush()
+            out.close()
+        }
+    }
+
+    override fun getByteArrayFromInputStream(inputStream: InputStream): ByteArray {
+        return ByteArray(inputStream.available()).also {
+            inputStream.read(it)
+            inputStream.close()
+        }
+    }
+}
