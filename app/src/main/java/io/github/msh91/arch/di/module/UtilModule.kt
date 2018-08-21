3368e592c7e24986efb19b845c34275de6d28c22
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2018-08-21 10:38:54 +0430

    Create dagger module for providing utils

---
+++ app/src/main/java/io/github/msh91/arch/di/module/UtilModule.kt
@@ -0,0 +1,43 @@
+package io.github.msh91.arch.di.module
+
+import dagger.Binds
+import dagger.Module
+import io.github.msh91.arch.util.connectivity.BaseConnectionManager
+import io.github.msh91.arch.util.connectivity.ConnectionManager
+import io.github.msh91.arch.util.device.BaseDeviceUtil
+import io.github.msh91.arch.util.device.DeviceUtilImpl
+import io.github.msh91.arch.util.providers.BaseResourceProvider
+import io.github.msh91.arch.util.providers.ResourceProvider
+import io.github.msh91.arch.util.providers.file.BaseFileProvider
+import io.github.msh91.arch.util.providers.file.FileProvider
+import javax.inject.Singleton
+
+@Module
+interface UtilModule {
+
+    /**
+     * provide main implementation of [BaseConnectionManager] to check connection status
+     */
+    @Binds
+    @Singleton
+    fun bindConnectionManager(connectionManager: ConnectionManager): BaseConnectionManager
+
+    /**
+     * provide main implementation of [BaseDeviceUtil] to access device shared data, unique identifiers, etc
+     */
+    @Binds
+    @Singleton
+    fun bindDeviceUtil(deviceUtilImpl: DeviceUtilImpl): BaseDeviceUtil
+
+    /**
+     * Provide main implementation of [BaseResourceProvider] to access app raw resources
+     */
+    @Binds
+    fun bindResourceProvider(resourceProvider: ResourceProvider): BaseResourceProvider
+
+    /**
+     * Provide main implementation of [BaseFileProvider]
+     */
+    @Binds
+    fun bindFileProvider(fileProvider: FileProvider): BaseFileProvider
+}
