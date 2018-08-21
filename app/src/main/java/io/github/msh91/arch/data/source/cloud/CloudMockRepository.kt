3f00d6c3e6d8e1c731ba4bb343d1dcfcb4b0376d
Mohammad sharifi <sharifi.m91@gmail.com>
2018-08-21 10:34:41 +0430

    Load mock data from json files in assets directory of debug build variant

--- app/src/main/java/io/github/msh91/arch/data/source/cloud/CloudMockRepository.kt
+++ app/src/main/java/io/github/msh91/arch/data/source/cloud/CloudMockRepository.kt
@@ -1,6 +1,10 @@
 package io.github.msh91.arch.data.source.cloud
 
+import com.google.gson.Gson
 import io.github.msh91.arch.domain.model.movie.Movie
+import io.github.msh91.arch.util.extension.fromJson
+import io.github.msh91.arch.util.providers.BaseResourceProvider
+import io.github.msh91.arch.util.providers.file.BaseFileProvider
 import io.reactivex.Flowable
 
 /**
@@ -8,13 +12,17 @@ import io.reactivex.Flowable
  *
  * <b>CAUTION : this implementation should be used only in debug mode</b>
  */
-class CloudMockRepository : BaseCloudRepository {
+class CloudMockRepository(
+        private val resourceProvider: BaseResourceProvider,
+        private val gson: Gson,
+        private val fileProvider: BaseFileProvider
+) : BaseCloudRepository {
 
     override fun getAllMovies(): Flowable<List<Movie>> {
-        return Flowable.just(listOf(
-                Movie(1, "Batman Begins!"),
-                Movie(2, "Batman: The Dark Knight"),
-                Movie(3, "Batman: The Dark Knight Rises")
-        ))
+        return Flowable.fromCallable {
+            val inputStream = resourceProvider.getAsset("movies.json")
+            val response = String(fileProvider.getByteArrayFromInputStream(inputStream))
+            gson.fromJson<List<Movie>>(response)
+        }
     }
 }
