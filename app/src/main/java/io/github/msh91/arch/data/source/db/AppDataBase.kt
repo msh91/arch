78f696ab2e27fab114f91a2e22ee1fc7bab88522
Mohammad Sharifi <sharifi.m91@gmail.com>
2018-08-31 21:08:36 +0430

    Upgrade database version. enable Room destructive migration

--- app/src/main/java/io/github/msh91/arch/data/source/db/AppDataBase.kt
+++ app/src/main/java/io/github/msh91/arch/data/source/db/AppDataBase.kt
@@ -2,15 +2,15 @@ package io.github.msh91.arch.data.source.db
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
-import io.github.msh91.arch.domain.model.movie.Movie
 import io.github.msh91.arch.data.source.db.dao.MovieDao
+import io.github.msh91.arch.domain.model.movie.Movie
 
 @Database(entities = [Movie::class], version = AppDataBase.VERSION)
 abstract class AppDataBase : RoomDatabase() {
 
     companion object {
         const val DB_NAME = "arch.db"
-        const val VERSION = 1
+        const val VERSION = 2
     }
 
     abstract fun movieDao(): MovieDao
