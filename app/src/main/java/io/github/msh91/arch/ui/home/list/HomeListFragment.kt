3b3f6081dd85e470f5984fe39d238a3edf9f1173
Mohammad Sharifi <sharifi.m91@gmail.com>
2018-08-31 21:11:43 +0430

    Show received movies in HomeListFragment

--- app/src/main/java/io/github/msh91/arch/ui/home/list/HomeListFragment.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/list/HomeListFragment.kt
@@ -1,9 +1,13 @@
 package io.github.msh91.arch.ui.home.list
 
+import android.graphics.Movie
 import io.github.msh91.arch.R
 import io.github.msh91.arch.databinding.FragmentHomeListBinding
+import io.github.msh91.arch.databinding.ItemMovieBinding
 import io.github.msh91.arch.ui.base.BaseFragment
 import io.github.msh91.arch.ui.base.ViewModelScope
+import io.github.msh91.arch.ui.base.adapter.SingleLayoutAdapter
+import io.github.msh91.arch.util.extension.observe
 
 class HomeListFragment : BaseFragment<HomeListViewModel, FragmentHomeListBinding>() {
     companion object {
@@ -15,4 +19,15 @@ class HomeListFragment : BaseFragment<HomeListViewModel, FragmentHomeListBinding
     override val viewModel: HomeListViewModel by getLazyViewModel(ViewModelScope.ACTIVITY)
     override val layoutId: Int = R.layout.fragment_home_list
 
+    override fun onViewInitialized(binding: FragmentHomeListBinding) {
+        super.onViewInitialized(binding)
+        binding.viewModel = viewModel
+        binding.adapter = SingleLayoutAdapter<Movie, ItemMovieBinding>(
+                R.layout.item_movie,
+                emptyList(),
+                viewModel
+        )
+
+        observe(viewModel.movies) { binding.adapter?.swapItems(it) }
+    }
 }
--- app/src/main/java/io/github/msh91/arch/ui/home/list/HomeListViewModel.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/list/HomeListViewModel.kt
@@ -2,10 +2,12 @@ package io.github.msh91.arch.ui.home.list
 
 import android.util.Log
 import io.github.msh91.arch.domain.model.movie.Movie
+import io.github.msh91.arch.domain.model.response.SuccessResponse
 import io.github.msh91.arch.domain.model.response.UseCaseResponse
 import io.github.msh91.arch.domain.usecase.movie.GetAllMoviesUseCase
 import io.github.msh91.arch.ui.base.BaseViewModel
 import io.github.msh91.arch.util.connectivity.BaseConnectionManager
+import io.github.msh91.arch.util.livedata.NonNullLiveData
 import javax.inject.Inject
 
 class HomeListViewModel @Inject constructor(
@@ -14,6 +16,8 @@ class HomeListViewModel @Inject constructor(
 ) : BaseViewModel(connectionManager) {
     private val TAG = HomeListViewModel::class.java.simpleName
 
+    val movies = NonNullLiveData<List<Movie>>(emptyList())
+
     init {
         getAllMoviesUseCase
                 .execute(compositeDisposable, this::getAllMoviesResponse, this::onTokenExpired)
@@ -21,5 +25,12 @@ class HomeListViewModel @Inject constructor(
 
     private fun getAllMoviesResponse(response: UseCaseResponse<List<Movie>>) {
         Log.d(TAG, "getAllMoviesResponse() called  with: response = [$response]")
+        when (response) {
+            is SuccessResponse -> movies.value = response.value
+        }
+    }
+
+    fun onItemClicked(movie: Movie) {
+        Log.d(TAG, "onItemClicked() called  with: movie = [$movie]")
     }
 }
