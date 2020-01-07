923340baf81d6f103936abd132dea590e83f3ec6
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2020-01-07 11:16:39 +0330

    Create buildSrc module to enable kotlin dsl and Define all app gradle dependencies and constants

---
+++ buildSrc/src/main/kotlin/io/github/msh91/arch/Android.kt
@@ -0,0 +1,10 @@
+package io.github.msh91.arch
+
+object Android {
+    const val minSdkVersion = 18
+    const val targetSdkVersion = 28
+    const val compileSdkVersion = 28
+    const val applicationId = "io.github.msh91.arch"
+    const val versionCode = 1
+    const val versionName = "0.1"
+}
