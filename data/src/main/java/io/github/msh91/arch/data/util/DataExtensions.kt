8265ababd5016774a4655de23e96c5b25e4038fd
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2020-04-25 11:54:47 +0430

    Define and extension function to convert LiveData<T> to LiveData<Either<Error, T>>

--- data/src/main/java/io/github/msh91/arch/data/util/DataExtensions.kt
+++ data/src/main/java/io/github/msh91/arch/data/util/DataExtensions.kt
@@ -1,8 +1,16 @@
 package io.github.msh91.arch.data.util
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import arrow.core.Either
 import com.google.gson.Gson
 import com.google.gson.JsonElement
+import io.github.msh91.arch.data.model.response.Error
 
 inline fun <reified T> Gson.fromJson(jsonElement: JsonElement): T? = this.fromJson<T>(jsonElement, object : com.google.gson.reflect.TypeToken<T>() {}.type)
 
 inline fun <reified T> Gson.fromJson(jsonStr: String): T? = this.fromJson<T>(jsonStr, object : com.google.gson.reflect.TypeToken<T>() {}.type)
+
+fun <T> LiveData<T>.toEither(): LiveData<Either<Error, T>> {
+    return Transformations.map(this) { input -> Either.Right(input) }
+}
