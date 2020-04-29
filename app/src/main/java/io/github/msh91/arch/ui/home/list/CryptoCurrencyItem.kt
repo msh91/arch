c7b6c496b5e9934a2ec6f6739bc8272dadf8ee1a
Mohammad Sharifi <sharifi.m91@gmail.com>
2020-04-29 12:05:16 +0430

    Request for list of crypto currencies and show them in the HomeListFragment

---
+++ app/src/main/java/io/github/msh91/arch/ui/home/list/CryptoCurrencyItem.kt
@@ -0,0 +1,8 @@
+package io.github.msh91.arch.ui.home.list
+
+data class CryptoCurrencyItem(
+        val name: String,
+        val price: String,
+        val percent: String,
+        val percentColor: Int
+)
