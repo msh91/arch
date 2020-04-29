9da6378f5c5978e0ccf8e3a78c5d9e627a59fe0c
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2020-04-29 12:05:16 +0430

    Request for list of crypto currencies and show them in the HomeListFragment

--- app/src/main/java/io/github/msh91/arch/ui/home/list/HomeListViewModel.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/list/HomeListViewModel.kt
@@ -4,22 +4,23 @@ import android.util.Log
 import androidx.lifecycle.viewModelScope
 import arrow.core.Either.Left
 import arrow.core.Either.Right
-import io.github.msh91.arch.data.mapper.ErrorMapper
+import io.github.msh91.arch.R
 import io.github.msh91.arch.data.model.movie.Movie
 import io.github.msh91.arch.data.model.Error
-import io.github.msh91.arch.data.repository.movie.MovieRepository
+import io.github.msh91.arch.data.model.crypto.CryptoCurrency
+import io.github.msh91.arch.data.repository.crypto.CryptoRepository
 import io.github.msh91.arch.ui.base.BaseViewModel
 import io.github.msh91.arch.util.livedata.NonNullLiveData
+import io.github.msh91.arch.util.providers.BaseResourceProvider
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class HomeListViewModel @Inject constructor(
-        private val movieRepository: MovieRepository,
-        errorMapper: ErrorMapper
+        private val cryptoRepository: CryptoRepository,
+        private val resourceProvider: BaseResourceProvider
 ) : BaseViewModel() {
-    private val TAG = HomeListViewModel::class.java.simpleName
 
-    val movies = NonNullLiveData<List<Movie>>(emptyList())
+    val cryptoCurrencies = NonNullLiveData<List<CryptoCurrencyItem>>(emptyList())
 
     init {
         getAllMovies()
@@ -27,10 +28,22 @@ class HomeListViewModel @Inject constructor(
 
     private fun getAllMovies() {
         viewModelScope.launch {
-            when (val either = movieRepository.getAllMovies()) {
-                is Right -> movies.value = either.b
+            when (val either = cryptoRepository.getLatestUpdates()) {
+                is Right -> cryptoCurrencies.value = mapCurrenciesToItems(either.b)
                 is Left -> showError(either.a)
             }
+        }
+    }
+
+    private fun mapCurrenciesToItems(currencies: List<CryptoCurrency>): List<CryptoCurrencyItem> {
+        return currencies.map { currency ->
+            val quote = currency.quotes["USD"] ?: error("USD quote is a must!")
+            CryptoCurrencyItem(
+                    currency.name,
+                    resourceProvider.getString(R.string.holder_usd_price, quote.price),
+                    resourceProvider.getString(R.string.holder_percent, quote.percentChange24h),
+                    resourceProvider.getColor(if (quote.percentChange24h < 0) R.color.red else R.color.green)
+            )
         }
     }
 
@@ -38,7 +51,11 @@ class HomeListViewModel @Inject constructor(
         Log.d(TAG, "showError() called  with: error = [$error]")
     }
 
-    fun onItemClicked(movie: Movie) {
-        Log.d(TAG, "onItemClicked() called  with: movie = [$movie]")
+    fun onItemClicked(cryptoCurrencyItem: CryptoCurrencyItem) {
+        Log.d(TAG, "onItemClicked() called  with: cryptoCurrencyItem = [$cryptoCurrencyItem]")
+    }
+
+    companion object {
+        private const val TAG = "HomeListViewModel"
     }
 }
