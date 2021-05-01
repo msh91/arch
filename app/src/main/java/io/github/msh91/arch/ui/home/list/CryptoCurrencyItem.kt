bf8d1627a09cede95078825f014807362633554e
Mohammad Sharifi <sharifi.m91@gmail.com>
2021-05-01 18:59:56 +0430

    Navigate user to CryptoChartFragment when user clicks only on bitcoin item

--- app/src/main/java/io/github/msh91/arch/ui/home/list/CryptoCurrencyItem.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/list/CryptoCurrencyItem.kt
@@ -1,6 +1,9 @@
 package io.github.msh91.arch.ui.home.list
 
+import io.github.msh91.arch.data.model.crypto.CryptoCurrency
+
 data class CryptoCurrencyItem(
+    val cryptoCurrency: CryptoCurrency,
     val name: String,
     val price: String,
     val percent: String,
