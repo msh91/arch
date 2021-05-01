32a5a05e7eda3927c4ad7ae7dd67ace0be18cf3f
Mohammad Sharifi <sharifi.m91@gmail.com>
2021-05-01 17:11:15 +0430

    Show a progressBar when trying to receive latest updates

--- app/src/main/java/io/github/msh91/arch/ui/home/list/LatestUpdatesFragment.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/list/LatestUpdatesFragment.kt
@@ -3,26 +3,26 @@ package io.github.msh91.arch.ui.home.list
 import androidx.lifecycle.Observer
 import com.google.android.material.snackbar.Snackbar
 import io.github.msh91.arch.R
-import io.github.msh91.arch.databinding.FragmentHomeListBinding
+import io.github.msh91.arch.databinding.FragmentLatestUpdatesBinding
 import io.github.msh91.arch.databinding.ItemCryptoCurrencyBinding
 import io.github.msh91.arch.ui.base.BaseFragment
 import io.github.msh91.arch.ui.base.ViewModelScope
 import io.github.msh91.arch.ui.base.adapter.SingleLayoutAdapter
 import io.github.msh91.arch.util.extension.observeSafe
 
-class LatestUpdatesFragment : BaseFragment<LatestUpdatesViewModel, FragmentHomeListBinding>() {
+class LatestUpdatesFragment : BaseFragment<LatestUpdatesViewModel, FragmentLatestUpdatesBinding>() {
 
     override val viewModel: LatestUpdatesViewModel by getLazyViewModel(ViewModelScope.FRAGMENT)
-    override val layoutId: Int = R.layout.fragment_home_list
+    override val layoutId: Int = R.layout.fragment_latest_updates
 
-    override fun onViewInitialized(binding: FragmentHomeListBinding) {
+    override fun onViewInitialized(binding: FragmentLatestUpdatesBinding) {
         super.onViewInitialized(binding)
         binding.viewModel = viewModel
         binding.adapter = SingleLayoutAdapter<CryptoCurrencyItem, ItemCryptoCurrencyBinding>(
             layoutId = R.layout.item_crypto_currency,
             onItemClicked = viewModel::onItemClicked
         )
-        viewModel.cryptoCurrencies.observeSafe(viewLifecycleOwner) { binding.adapter?.swapItems(it) }
+        viewModel.cryptoCurrencyItemsLiveData.observeSafe(viewLifecycleOwner) { binding.adapter?.swapItems(it) }
         viewModel.errorLiveData.observe(viewLifecycleOwner, Observer { showSnackBar(it) })
     }
 
