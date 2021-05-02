ce45ead4039a2616093342f06c74ae719082b3d2
Mohammad Sharifi <sharifi.m91@gmail.com>
2021-05-02 22:50:02 +0430

    Receive chartInfo and Show it via AnyChartView

--- app/src/main/java/io/github/msh91/arch/ui/home/chart/ChartInfoItem.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/chart/ChartInfoItem.kt
@@ -3,7 +3,13 @@ package io.github.msh91.arch.ui.home.chart
 import com.anychart.chart.common.dataentry.ValueDataEntry
 
 data class ChartInfoItem(
-    val entries: List<ValueDataEntry>,
     val name: String,
-    val description: String
+    val description: String,
+    val entries: List<ChartEntry>
 )
+
+data class ChartEntry(val x: String, val y: Float)
+
+fun ChartEntry.toDataEntry(): ValueDataEntry {
+    return ValueDataEntry(x, y)
+}
