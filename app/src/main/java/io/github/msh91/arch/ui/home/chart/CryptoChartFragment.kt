bfb92e5b67ddd2a960d0b509bfecaf07535af043
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2021-05-02 22:50:02 +0430

    Receive chartInfo and Show it via AnyChartView

--- app/src/main/java/io/github/msh91/arch/ui/home/chart/CryptoChartFragment.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/chart/CryptoChartFragment.kt
@@ -1,9 +1,17 @@
 package io.github.msh91.arch.ui.home.chart
 
+import androidx.lifecycle.Observer
+import com.anychart.AnyChart
+import com.anychart.charts.Cartesian
+import com.anychart.enums.Anchor
+import com.anychart.enums.MarkerType
+import com.anychart.enums.TooltipPositionMode
 import io.github.msh91.arch.R
 import io.github.msh91.arch.databinding.FragmentCryptoChartBinding
 import io.github.msh91.arch.ui.base.BaseFragment
 import io.github.msh91.arch.ui.base.ViewModelScope
+import io.github.msh91.arch.util.extension.showSnackBar
+
 
 class CryptoChartFragment : BaseFragment<CryptoChartViewModel, FragmentCryptoChartBinding>() {
 
@@ -13,6 +21,35 @@ class CryptoChartFragment : BaseFragment<CryptoChartViewModel, FragmentCryptoCha
 
     override fun onViewInitialized(binding: FragmentCryptoChartBinding) {
         super.onViewInitialized(binding)
+        binding.viewModel = viewModel
+        viewModel.chartInfoLiveData.observe(viewLifecycleOwner, Observer { showDataEntries(it) })
+        viewModel.errorLiveData.observe(viewLifecycleOwner, Observer { showSnackBar(it) })
+    }
 
+    private fun showDataEntries(chartInfoItem: ChartInfoItem) {
+        val cartesian: Cartesian = AnyChart.line()
+        cartesian.animation(true)
+        cartesian.padding(10.0, 20.0, 5.0, 20.0)
+        cartesian.crosshair().enabled(true)
+        cartesian.crosshair().yLabel(false)
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT)
+        cartesian.title(chartInfoItem.description)
+        cartesian.xAxis(0).labels().padding(5.0, 5.0, 5.0, 5.0)
+        cartesian.xAxis(0).title("Last 60 Days")
+        val line = cartesian.line(chartInfoItem.entries.map { it.toDataEntry() })
+        line.name(chartInfoItem.name)
+        line.hovered().markers().enabled(true)
+        line.hovered().markers()
+            .type(MarkerType.CIRCLE)
+            .size(4.0)
+        line.tooltip()
+            .position("right")
+            .anchor(Anchor.LEFT_CENTER)
+            .offsetX(5.0)
+            .offsetY(5.0)
+        cartesian.legend().enabled(true)
+        cartesian.legend().fontSize(13.0)
+        cartesian.legend().padding(0.0, 0.0, 10.0, 0.0)
+        binding.lineChart.setChart(cartesian)
     }
 }
--- app/src/main/java/io/github/msh91/arch/ui/home/chart/CryptoChartViewModel.kt
+++ app/src/main/java/io/github/msh91/arch/ui/home/chart/CryptoChartViewModel.kt
@@ -1,9 +1,61 @@
 package io.github.msh91.arch.ui.home.chart
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import arrow.core.Either
+import io.github.msh91.arch.R
+import io.github.msh91.arch.data.model.Error
+import io.github.msh91.arch.data.model.crypto.CryptoChartInfo
+import io.github.msh91.arch.data.repository.crypto.CryptoChartRepository
 import io.github.msh91.arch.ui.base.BaseViewModel
+import io.github.msh91.arch.util.livedata.SingleEventLiveData
+import io.github.msh91.arch.util.provider.BaseResourceProvider
+import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
-class CryptoChartViewModel @Inject constructor() : BaseViewModel() {
+class CryptoChartViewModel @Inject constructor(
+    private val cryptoChartRepository: CryptoChartRepository,
+    private val resourceProvider: BaseResourceProvider
+) : BaseViewModel() {
+
+    val chartInfoLiveData = MutableLiveData<ChartInfoItem>()
     val loadingLiveData = MutableLiveData<Boolean>()
+    val errorLiveData = SingleEventLiveData<String>()
+    var callInitialized = false
+
+    override fun onStart() {
+        super.onStart()
+        if (!callInitialized) {
+            getBitcoinChart()
+            callInitialized = true
+        }
+    }
+
+    private fun getBitcoinChart() {
+        viewModelScope.launch {
+            loadingLiveData.value = true
+            when (val either = cryptoChartRepository.getChartInfo()) {
+                is Either.Left -> showError(either.a)
+                is Either.Right -> showChartInfo(either.b)
+            }
+            loadingLiveData.value = false
+        }
+    }
+
+    private fun showChartInfo(chartInfo: CryptoChartInfo) {
+        val entries = chartInfo.chartValues.map { chartValue ->
+            val calendar = Calendar.getInstance()
+            calendar.time = chartValue.date
+            val monthName = calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault())
+            val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+            val x = resourceProvider.getString(R.string.holder_day_month, monthName, dayOfMonth)
+            ChartEntry(x, chartValue.price)
+        }
+        chartInfoLiveData.value = ChartInfoItem(chartInfo.name, chartInfo.description, entries)
+    }
+
+    private fun showError(error: Error) {
+        errorLiveData.value = resourceProvider.getErrorMessage(error)
+    }
 }
