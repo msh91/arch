6d4b09879154527c9df1ce8949e24aefeca8cd87
Mo Sharifi <29056024+msh91@users.noreply.github.com>
2024-02-21 11:57:00 +0100

    Update Gradle, Compile SDK, and dependencies

--- buildSrc/src/main/kotlin/io/github/msh91/arch/Android.kt
+++ buildSrc/src/main/kotlin/io/github/msh91/arch/Android.kt
@@ -1,9 +1,9 @@
 package io.github.msh91.arch
 
 object Android {
-    const val minSdkVersion = 21
-    const val targetSdkVersion = 28
-    const val compileSdkVersion = 28
+    const val minSdkVersion = 23
+    const val targetSdkVersion = 34
+    const val compileSdkVersion = 34
     const val applicationId = "io.github.msh91.arch"
     const val versionCode = 1
     const val versionName = "0.1"
--- buildSrc/src/main/kotlin/io/github/msh91/arch/Libs.kt
+++ buildSrc/src/main/kotlin/io/github/msh91/arch/Libs.kt
@@ -4,7 +4,6 @@ object Libs {
     object Plugins {
         const val gradle = "com.android.tools.build:gradle:${Versions.gradle}"
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
-        const val gradleVersions = "com.github.ben-manes:gradle-versions-plugin:${Versions.gradleVersions}"
         const val ktlint = "com.pinterest:ktlint:${Versions.ktlint}"
         const val safeargs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigationComponent}"
     }
@@ -14,17 +13,16 @@ object Libs {
     }
 
     object Jetpack {
-        const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.arch}"
-        const val lifecycleKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.arch}"
-        const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.arch}"
-        const val livedataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.arch}"
+        const val lifecycleKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle}"
+        const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+        const val livedataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
         const val room = "androidx.room:room-runtime:${Versions.room}"
         const val roomKtx = "androidx.room:room-ktx:${Versions.room}"
         const val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
         const val material = "com.google.android.material:material:${Versions.material}"
         const val annotations = "androidx.annotation:annotation:${Versions.material}"
         const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
-        const val navigationUi ="androidx.navigation:navigation-ui:${Versions.navigationComponent}"
+        const val navigationUi = "androidx.navigation:navigation-ui:${Versions.navigationComponent}"
         const val navigationFragment = "androidx.navigation:navigation-fragment:${Versions.navigationComponent}"
         const val fragment = "androidx.fragment:fragment-ktx:${Versions.fragment}"
     }
@@ -34,8 +32,6 @@ object Libs {
         const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
         const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
         const val arrowCore = "io.arrow-kt:arrow-core:${Versions.arrow}"
-        const val arrowSyntax = "io.arrow-kt:arrow-syntax:${Versions.arrow}"
-        const val arrowMeta = "io.arrow-kt:arrow-meta:${Versions.arrow}"
         const val dagger = "com.google.dagger:dagger:${Versions.dagger}"
         const val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
         const val daggerAndroid = "com.google.dagger:dagger-android-support:${Versions.dagger}"
