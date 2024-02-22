17328d9ba622b989269df99a1602f7eec3fab912
Mohammad Sharifi <sharifi.m91@gmail.com>
2024-02-22 17:47:30 +0100

    Migrate build scripts from Groovy to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.safeargs) apply false
+    alias(libs.plugins.ktlint) apply true
+}
+
