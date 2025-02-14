c572fa89662a5ae463372c5b8d5807f1e228a75f
Mohammad Sharifi <swesharifi@gmail.com>
2025-02-14 09:45:13 +0100

    Refactor GetLatestPriceUseCase to use `currentCoroutineContext().isActive` instead of unsafe `while(true)` loop for checking the coroutine active status

--- feature/history/impl/src/main/kotlin/io/github/msh91/arcyto/history/domain/usecase/GetLatestPriceUseCase.kt
+++ feature/history/impl/src/main/kotlin/io/github/msh91/arcyto/history/domain/usecase/GetLatestPriceUseCase.kt
@@ -5,14 +5,11 @@ import io.github.msh91.arcyto.core.di.scope.AppScope
 import io.github.msh91.arcyto.history.data.repository.HistoricalChartRepository
 import io.github.msh91.arcyto.history.domain.model.LatestPrice
 import io.github.msh91.arcyto.history.domain.model.LatestPriceRequest
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.isActive
 import javax.inject.Inject
 
 interface GetLatestPriceUseCase {
@@ -29,9 +26,8 @@ class GetLatestPriceUseCaseImpl @Inject constructor(
 
     override suspend fun invoke(request: LatestPriceRequest): Flow<Result<LatestPrice>> {
         // Fetch the latest price in an interval provided by the request
-        val coroutineScope = CoroutineScope(currentCoroutineContext() + SupervisorJob())
         return flow {
-            while (true) {
+            while (currentCoroutineContext().isActive) {
                 emit(
                     historicalChartRepository.getLatestCoinPrice(
                         coinId = request.coinId,
@@ -41,10 +37,6 @@ class GetLatestPriceUseCaseImpl @Inject constructor(
                 )
                 delay(request.intervalMs)
             }
-        }.shareIn(
-            scope = coroutineScope,
-            started = SharingStarted.WhileSubscribed(),
-            replay = 1,
-        )
+        }
     }
 }
